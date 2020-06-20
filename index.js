ec5afae4b9aaa64995c0bc3c5d83b5a3216d9afe
Aru Gyani <aru@gyani.com>
2020-06-20 12:17:52 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+const main = async() => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+        deviceScaleFactor: 1,
+    });
+
+    await page.goto('https://unsplash.com');
+    await page.screenshot({path: 'unsplash.png'});
+
+    await browser.close();
+}
+
+main();
